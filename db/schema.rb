7fe6ea9123f66e587ff3d44da582ef4911f6c0df
Josh Tukman <josh.tukman@gmail.com>
2020-09-09 17:02:11 -0600

    Updated schema reflecting email attribute for user resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_033446) do
+ActiveRecord::Schema.define(version: 2020_09_09_043735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_033446) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.string "email"
   end
 
 end
