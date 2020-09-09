1337123d6214f68372000d3b06865f4900dc03fe
Josh Tukman <josh.tukman@gmail.com>
2020-09-08 22:01:28 -0600

    Add spotify_request action for making initial spotify api call

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::AuthController < ApplicationController
+  def spotify_request
+    url = "https://accounts.spotify.com/authorize"
+    query_params = {
+      client_id: ENV['SPOTIFY_CLIENT_ID'],
+      response_type: 'code',
+      redirect_uri: 'http://localhost:3000/api/v1/user',
+      scope: "user-library-read
+      playlist-read-collaborative
+      playlist-modify-private
+      user-modify-playback-state
+      user-read-private
+      user-top-read
+      playlist-modify-public
+      user-read-recently-played
+      user-read-currently-playing
+      user-read-playback-state",
+     show_dialog: true
+    }
+    redirect_to "#{url}?#{query_params.to_query}"
+  end
+end 
