008a4ea99e6c36ad84aefdecec88dc7a048a895a
Sienna Kopf <princess.kopf@gmail.com>
2020-09-10 11:10:10 -0600

    Remove default user namespace and replace with dashboard path, dashboard controller, and index action:

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  
+
   root 'welcome#index'
-  
+
   get '/login', to: 'auth#spotify_request'
   get '/auth', to: 'auth#show'
   get '/user', to: 'users#create'
   patch '/user', to: 'users#update'
-  
-  namespace :default_user do
-    get '/:id', to: 'profile#index'
-  end
+
+  get '/dashboard', to: 'dashboard#index'
 end
