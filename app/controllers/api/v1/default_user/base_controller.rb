fe410d63cb79cab0f85c0f6f05af8e5d7563ff06
Josh Tukman <josh.tukman@gmail.com>
2020-09-09 23:17:44 -0600

    Remove unnecessary namespaced routes

--- app/controllers/api/v1/default_user/base_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::DefaultUser::BaseController < ApplicationController
-  before_action :require_default_user
-
-  def require_default_user
-      render file: "/public/404" unless current_default_user?
-  end
-end 
--- app/controllers/api/v1/default_user/profile_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::DefaultUser::ProfileController < Api::V1::DefaultUser::BaseController
-  def index
-  
-  end
-end 
