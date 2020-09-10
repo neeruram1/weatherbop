b3c96cd4b9c7542c9dc433e80145b0c16058bf5b
Sienna Kopf <princess.kopf@gmail.com>
2020-09-10 11:47:01 -0600

    Add test for weather API call

---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe WeatherService do
+  context "instance methods" do
+    context "#forecast" do
+      it "returns current weather data for a particular location" do
+        service = WeatherService.new
+        search = service.forecast("denver, co, usa")
+        expect(search).to be_a Hash
+        expect(search).to have_key :weather
+        expect(search[:weather]).to be_an Array
+        expect(search[:weather][0]).to be_a Hash
+        expect(search[:weather][0]).to have_key :id
+        expect(search[:weather].first).to have_key :main
+        expect(search[:weather].first).to have_key :description
+        expect(search[:weather].first).to have_key :icon
+
+        expect(search).to have_key :main
+        expect(search[:main]).to be_an Hash
+        expect(search[:main]).to have_key :temp
+        expect(search[:main]).to have_key :temp_min
+        expect(search[:main]).to have_key :temp_max
+
+        expect(search).to have_key :wind
+        expect(search[:wind]).to be_a Hash
+
+        expect(search[:name]).to eq("Denver")
+      end
+    end
+  end
+end
